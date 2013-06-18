192da7adbd0b1cae94a024841943f50447579b80
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2013-06-19 00:54:26 +0530

    Adding the actual class period

---
+++ RadiographyTracking/RadiographyTracking.Web/Models/Period.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RadiographyTracking.Web.Models
+{
+    public class Period 
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public int FoundryID { get; set; }
+
+        public Foundry Foundry { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
